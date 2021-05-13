88a93c8561e665b0846139cd012e11b0254aa716
tejak <180031006@kluniversity.in>
2021-05-13 10:57:02 +0530

    Array Problems

---
+++ ArraysProblems/src/arrays/NumberOfElements.java
@@ -0,0 +1,23 @@
+
+package arrays;
+import java.util.*;
+public class NumberOfElements {
+
+
+  @SuppressWarnings("unused")
+public static void main(String[] args) {
+    @SuppressWarnings("resource")
+	Scanner sc = new Scanner(System.in);
+    System.out.print("Enter number of elements ");
+    int n = sc.nextInt();
+    int[] a =new int[n];
+    int i,j;
+    for(i=0;i<n;i++) 
+    {
+      a[i]=sc.nextInt();
+    }
+  }
+
+
+
+}
