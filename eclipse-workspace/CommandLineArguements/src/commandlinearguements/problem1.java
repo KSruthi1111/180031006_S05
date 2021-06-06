3634f01ad53c11b473816208e599f3c8330fea8e
tejak <180031006@kluniversity.in>
2021-06-06 20:01:30 +0530

    Command Line Arguement

---
+++ eclipse-workspace/CommandLineArguements/src/commandlinearguements/problem1.java
@@ -0,0 +1,12 @@
+package commandlinearguements;
+
+public class problem1 {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(args[0]+" "+"Technologies"+" "+args[1]);
+
+	}
+
+}
---
+++ eclipse-workspace/CommandLineArguements/src/commandlinearguements/program2.java
@@ -0,0 +1,11 @@
+package commandlinearguements;
+
+public class program2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Welcome"+" "+args[0]);
+
+	}
+
+
+}
